082f12d22f3a6640c891fa1ecd0064df3b214476
Tyler-Hilbert <TylerJoshuaHilbert@gmail.com>
2016-10-03 10:12:32 -0700

    Added choice for higher index of refraction

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+var greaterIndex = 1; // The medium with the greater index
+
 function swap() {
-	alert ("SWap");
+	if (greaterIndex == 1) {
+		document.getElementById("layer1").setAttribute("d", "M 0, 0 L300,200");
+		document.getElementById("layer2").setAttribute("d", "M300, 0 L400,200");
+		document.getElementById("layer3").setAttribute("d", "M400, 0 L700,200");
+		document.getElementById("description").innerHTML = "When medium 1 has a lower index of refraction than medium 2";
+		greaterIndex = 2;
+	} else {
+		document.getElementById("layer1").setAttribute("d", "M 0, 0 L100,200");
+		document.getElementById("layer2").setAttribute("d", "M100, 0 L400,200");
+		document.getElementById("layer3").setAttribute("d", "M400, 0 L500,200");
+		document.getElementById("description").innerHTML = "When medium 1 has a higher index of refraction than medium 2";
+		greaterIndex = 1;
+	} 
 }
