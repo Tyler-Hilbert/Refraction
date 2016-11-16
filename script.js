b489d7a7dc4cc90519104fb36be25d3f2be7a930
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2016-11-16 11:37:26 -0500

    Moved calculator class to its own file

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
+// IMPORTANT: make sure to include calculator.js before this class
+
 var greaterIndex = 1; // The medium with the greater index
-
-// Calculator class
-function Calculator(ni, nr, aoi) {
-	this.ni = ni;
-	this.nr = nr;
-	this.aoi = aoi;
-
-	this.getAOR = function() {
-		aoiRadians = this.degreesToRadians(aoi);
-		aorRadians = Math.asin(ni * Math.sin(aoiRadians) / nr);
-		aorDegrees = this.radiansToDegrees(aorRadians);
-		return aorDegrees;
-	};
-
-	this.getCriticalAngle = function() {
-		criticalAngleRadians = Math.asin(nr / ni);
-		criticalAngleDegrees = this.radiansToDegrees(criticalAngleRadians);
-		return criticalAngleDegrees;
-	};
-
-	this.radiansToDegrees = function(radians) {
-		return radians * (180/Math.PI);
-	};
-
-	this.degreesToRadians = function(degrees) {
-		return degrees * (Math.PI/180);
-	};
-};
-
 
 function swap() {
 	if (greaterIndex == 1) {
