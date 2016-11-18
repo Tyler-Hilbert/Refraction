20ce6a9edf469140a4085fcfa4d1fb6b3309ce49
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2016-11-18 13:35:38 -0500

    Added calculator in test class

--- test/test.js
+++ test/test.js
@@ -1,10 +1,26 @@
-var app = require('../calculator.js')
+var fs = require('fs');
+var vm = require('vm');
+var path = './calculator.js';
+var code = fs.readFileSync(path);
+vm.runInThisContext(code);
 
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+
+      for (var aoi = 10; aoi <= 90; aoi+=10) {
+        for (var nr = .5; nr <= 4; nr+= .5) {
+          for (var ni = .5; ni <= 4; ni+= .5) {
+            var calc = new Calculator(ni, nr, aoi);
+            console.log("ni: " + ni + " nr: " + nr + " aoi (in degrees): " + aoi);
+            console.log("Angle of refraction: " + calc.getAOR());
+            console.log("Critical angle: " + calc.getCriticalAngle());
+          }
+        }
+      }
+
+      //assert.equal(-1, [1,2,3].indexOf(4));
     });
   });
 });
